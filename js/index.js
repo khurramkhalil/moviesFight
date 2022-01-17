9e065e5dfc82fb5d9031bb26ee9ee190c3522c9a
Khurram Khalil <kkhalil.pg@smme.edu.pk>
2022-01-17 03:56:08 -0800

    Got api keys, implemented movie search on input change, added gaurd feature to limit generated requests.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,27 @@
-console.log("Hi There!!")
+// apiKey: 62d85de2
+
+const fetchData = async (searchTerm) => {
+  const response = await axios.get('http://www.omdbapi.com/', {
+    params: {
+      apikey: '62d85de2',
+      s: searchTerm,
+      // i: "tt0848228"
+    }
+  });
+  console.log(response.data)
+};
+
+
+let timeoutId;
+const onInput = (event) => {
+  if (timeoutId) {
+    clearTimeout(timeoutId);
+  }
+  timeoutId = setTimeout(() => {
+    fetchData(event.target.value)
+  }, 500)
+
+};
+
+const input = document.querySelector('input');
+input.addEventListener('input', onInput)
