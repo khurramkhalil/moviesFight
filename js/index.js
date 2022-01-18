d4953cded365ab539de25aa490ee39972d930844
Khurram Khalil <kkhalil.pg@smme.edu.pk>
2022-01-18 01:30:58 -0800

    Dropdown menus is dynamic now, clicks event listeners are added to handle 'is-active' class of dropdown menu.

--- js/index.js
+++ js/index.js
@@ -15,7 +15,7 @@ const fetchData = async (searchTerm) => {
 
 const root = document.querySelector('.autocomplete')
 root.innerHTML = `
-    <label><b>Search For a Movie</b></label>label>
+    <label><b>Search For a Movie</b></label>
     <input class="input" />
     <div class="dropdown">
         <div class="dropdown-menu">
@@ -31,23 +31,42 @@ const resultsWraper = document.querySelector('.results')
 
 const onInput = async (event) => {
   const movies = await fetchData(event.target.value)
+
+  if (!movies.length){
+    dropdown.classList.remove('is-active')
+    return;
+
+  }
+
   resultsWraper.innerHTML = '';
   for (let movie of movies) {
 
     dropdown.classList.add('is-active')
     const option = document.createElement('a')
 
-    const imgSrc = movie.Poster === 'N/A'? '' : movie.Poster
+    const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster
     option.classList.add('dropdown-item')
     option.innerHTML = `
     <img src="${imgSrc}" />
     ${movie.Title}
     `;
+    option.addEventListener('click', ()=> {
+      dropdown.classList.remove('is-active')
+      input.value = movie.Title;
+    })
 
     resultsWraper.appendChild(option);
   }
 
 };
 
-
 input.addEventListener('input', debounce(onInput, 500))
+
+// To close down the dropdown menu. We will listen for the click in entire document
+// if this click is other than the root, we will close 'is-active' class from dropdown.
+
+document.addEventListener('click', (event) => {
+  if (!root.contains(event.target)) {
+    dropdown.classList.remove('is-active');
+  }
+})
