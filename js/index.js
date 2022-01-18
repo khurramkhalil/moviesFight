9e4552618510e0c5f00e49c949eabd5cca2b2d3c
Khurram Khalil <kkhalil.pg@smme.edu.pk>
2022-01-18 00:00:10 -0800

    Added dynamic html elements for searched movies list, working dropdown with different checks.

--- js/index.js
+++ js/index.js
@@ -7,15 +7,46 @@ const fetchData = async (searchTerm) => {
     }
   });
 
+  if (response.data.Error) {
+    return [];
+  }
   return response.data.Search;
 };
 
+const root = document.querySelector('.autocomplete')
+root.innerHTML = `
+    <label><b>Search For a Movie</b></label>label>
+    <input class="input" />
+    <div class="dropdown">
+        <div class="dropdown-menu">
+            <div class="dropdown-content results"></div>
+        </div>
+    </div>
+`;
+
 const input = document.querySelector('input');
+const dropdown = document.querySelector('.dropdown')
+const resultsWraper = document.querySelector('.results')
 
 
 const onInput = async (event) => {
   const movies = await fetchData(event.target.value)
-  console.log(movies)
+  resultsWraper.innerHTML = '';
+  for (let movie of movies) {
+
+    dropdown.classList.add('is-active')
+    const option = document.createElement('a')
+
+    const imgSrc = movie.Poster === 'N/A'? '' : movie.Poster
+    option.classList.add('dropdown-item')
+    option.innerHTML = `
+    <img src="${imgSrc}" />
+    ${movie.Title}
+    `;
+
+    resultsWraper.appendChild(option);
+  }
+
 };
 
 
